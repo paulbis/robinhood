379bdff4e2545654e4a24ed0247c409e51b96eb1
guillaume <guillaume@guillaume-VPCZ21C5E>
2014-03-31 16:55:56 +0200

    Test service controller

--- src/PaulB/RobinHood/Controller/DestinationController.php
+++ src/PaulB/RobinHood/Controller/DestinationController.php
@@ -4,5 +4,33 @@ namespace PaulB\RobinHood\Controller;
 
 class DestinationController extends Controller
 {
+    public function rootOffersAction()
+    {
+        $root = $this->container['destinations']->getRoot();
+        $page = $this->container['request']->get('page', 1);
+        
+        return $this->commonOffersAction($root, $this->container['destinations']->getChildren(), $page, 'root.twig');
+    }
     
+    public function destinationOffersAction()
+    {
+        $root = $this->container['destinations']->getChild($this->container['request']->get('destination_id'));
+        $page = $this->container['request']->get('page', 1);
+        
+        return $this->commonOffersAction($root, $this->container['destinations']->getChildren(), $page, 'root.twig');
+    }
+    
+    private function commonOffersAction($destination, $destinations, $page, $template)
+    {
+        return $this->container['twig']->render(
+            $template, array(
+                'destination' => $destination,
+                'destinations' => $destinations,
+                'offers' => $this->container['client']->getRooms(array_merge(array(
+                    'page' => (int) $page,
+                    'per_page' => 20,
+                ), $destination['api_params'])),
+            )
+        );
+    }
 }
--- src/PaulB/RobinHood/Controller/OfferController.php
+++ src/PaulB/RobinHood/Controller/OfferController.php
@@ -4,5 +4,27 @@ namespace PaulB\RobinHood\Controller;
 
 class OfferController extends Controller
 {
+    public function detailsAction()
+    {
+        $offer = $this->getOffer();
+        $destination = $this->container['destinations']->geocode($offer['location']['location'][1], $offer['location']['location'][0]);
+
+        return $this->container['twig']->render('details.twig', array(
+            'offer' => $offer,
+            'destination' => $destination,
+            'destinations' => $this->container['destinations']->getChildren(),
+        ));
+    }
     
+    public function redirectAction()
+    {
+        $offer = $this->getOffer();
+        
+        return $this->container->redirect($offer['url']);
+    }
+    
+    private function getOffer()
+    {
+        return $this->container['client']->getRoom($this->container['request']->get('offer_id'));
+    }
 }
